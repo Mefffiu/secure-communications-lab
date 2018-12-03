0e69dc62bf5d3be4878a9a4a7ce1c5359594d129
Mateusz Rybka <mefffiu@gmail.com>
2018-12-03 21:47:28 +0100

    initial version

---
+++ app.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const opts = {
+  key: fs.readFileSync('server-priv-key.pem'),
+  cert: fs.readFileSync('server-cert.pem'),
+  requestCert: true,
+  rejectUnauthorized: false,
+  ca: [ fs.readFileSync('server-cert.pem') ]
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`<a href="authenticate">Log in using client certificate</a>`);
+});
+
+app.get('/authenticate', (req, res) => {
+  const cert = req.connection.getPeerCertificate();
+  if (req.client.authorized) {
+    res.status(200)
+      .send(`<div>
+              <a>Authentication successful, your certificate:</a>
+              <a>Common Name: ${cert.subject.CN}</a>
+              <a>issued by ${cert.issuer.CN}</a>
+            </div>`);
+  } else if (cert.subject) {
+    res.status(403)
+      .send(`<div>
+              <a>Authentication failed, your certificate:</a>
+              <a>Common Name: ${cert.subject.CN}</a>
+              <a>issued by ${cert.issuer.CN}</a>
+          </div>`);
+  } else {
+    res.status(401)
+      .send(`<div>Authentication failed, no certificate found.</div>`);
+  }
+});
+
+https.createServer(opts, app).listen(9443);
